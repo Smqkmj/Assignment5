562ae58c67abb13f2bb2abe0e05eb825338eedef
Smqkmj <gotenks.ssj499@gmail.com>
2022-01-07 22:33:32 -0500

    started add column function

---
+++ script-add-column.js
@@ -0,0 +1,14 @@
+
+// ●	add columns to the grid
+$( ".add-col" ).click(function() {
+    
+    console.log("add column button clicked");
+  });
+// ●	remove rows from the grid
+// ●	remove columns from the grid
+// ●	select a color from a dropdown menu of colors
+// ●	click on a single cell, changing its color to the currently selected color
+// ●	fill all uncolored cells with the currently selected color
+// ●	fill all cells with the currently selected color
+// ●	clear all cells/restore all cells to their original/initial color
+// ●	click and hold (mouseover) from a single cell (start) to a different cell (end) such that all affected/hovered-over cells from start to end change to the currently selected color
