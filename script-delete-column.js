da2396d01a716a93a04101a35a98b8ea0cf4da3d
Smqkmj <gotenks.ssj499@gmail.com>
2022-01-09 12:30:57 -0500

    stuck!

--- script-delete-column.js
+++ script-delete-column.js
@@ -1,5 +1,9 @@
 // ●	remove columns from the grid
+const colParent=document.querySelectorAll('.row')
 $( ".del-col" ).click(function() {
     
     console.log("delete column button clicked");
-  });
+ console.log(colParent)
+     colParent.removeChild(colParent.lastElementChild) 
+    // $( ".mt-5" ).find( colParent.lastElementChild).removeChild();
+  })
